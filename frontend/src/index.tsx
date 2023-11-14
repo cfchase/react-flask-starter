b23c3da98d880e905ed2d4b3e1b7fdc882a60e68
Chris Chase <cchase@redhat.com>
2023-11-14 17:21:13 -0500

    WIP

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from '@app/index';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (process.env.NODE_ENV !== "production") {
+  const config = {
+    rules: [
+      {
+        id: 'color-contrast',
+        enabled: false
+      }
+    ]
+  };
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+  const axe = require("react-axe");
+  axe(React, ReactDOM, 1000, config);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
